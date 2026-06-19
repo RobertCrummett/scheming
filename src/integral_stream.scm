f30b92b856925c1c5bdfe658c1d86538e2a4e4e4
Robert Crummett <robertcrummett@robertcrummett.com>
2026-06-18 19:05:26 -0600

    Integral stream updated.

--- src/integral_stream.scm
+++ src/integral_stream.scm
@@ -17,20 +17,33 @@
 (define (scale-stream stream factor)
   (stream-map (lambda (x) (* x factor)) stream))
 
+;;; IMPLEMENTATION ONE
 ;;; Redefinition of integral to expect an integrand stream as a
 ;;; delayed argument.
-(define (integral delayed-integrand initial-value dt)
-  (define int
-    (cons-stream
-      initial-value
-      (let ((integrand (force delayed-integrand)))
-	(add-streams (scale-stream integrand dt) int))))
-  int)
+;;; (define (integral delayed-integrand initial-value dt)
+  ;;; (define int
+    ;;; (cons-stream
+      ;;; initial-value
+      ;;; (let ((integrand (force delayed-integrand)))
+	;;; (add-streams (scale-stream integrand dt) int))))
+  ;;; int)
 
 (define (solve f y0 dt)
   (define y (integral (delay dy) y0 dt))
   (define dy (stream-map f y))
   y)
+
+;;; IMPLEMENTATION TWO
+(define (integral delayed-integrand initial-value dt)
+  (cons-stream
+    initial-value
+    (let ((integrand (force delayed-integrand)))
+      (if (stream-null? integrand)
+	the-empty-stream
+	(integral (stream-cdr integrand)
+		  (+ (* dt (stream-car integrand))
+		     initial-value)
+		  dt)))))
 
 ;;; Approximate e = 2.718...
 ;;;
@@ -41,9 +54,11 @@
     (stream-car stream)
     (stream-ref (stream-cdr stream) (- n 1))))
 
-;;; TODO Will not work because of how racket handles internal definitions.
+;;; TODO Will not work with either definition of integral because
+;;; of how racket handles internal definitions.
 (display
   (stream-ref (solve (lambda (y) y)
 		     1
 		     0.001)
 	      1000))
+
